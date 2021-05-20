8d807e2bb35dfc72c6c2de2df6c5ede531f396c9
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-05-20 20:24:20 +0300

    My first problems from the Programming Fundamentals course.

---
+++ Programming-Fundamentals/basicSyntax/10. MultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10._MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int product = i * n;
+                Console.WriteLine($"{n} X {i} = {product}");
+            }
+        }
+    }
+}
