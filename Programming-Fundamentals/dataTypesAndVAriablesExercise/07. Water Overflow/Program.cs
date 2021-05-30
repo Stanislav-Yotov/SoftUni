1b81a4d88dfad1009053a44cc645862ee86de064
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-05-30 19:19:59 +0300

    Exercise from Data types and variables.

---
+++ Programming-Fundamentals/dataTypesAndVAriablesExercise/07. Water Overflow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int inputLitters = int.Parse(Console.ReadLine());
+                if (sum + inputLitters > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+                sum += inputLitters;
+            }
+
+            Console.WriteLine(sum);
+
+
+
+
+
+        }
+    }
+}
