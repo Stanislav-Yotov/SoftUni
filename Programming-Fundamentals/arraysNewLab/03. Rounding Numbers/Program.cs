cdc00c3c974687ba6783aac7c815ac29a770b3be
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-06-05 12:57:39 +0300

    My first encounter with arrays.

---
+++ Programming-Fundamentals/arraysNewLab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                                      .Split()
+                                      .Select(double.Parse)
+                                      .ToArray();
+
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int roundedNum = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+                if (numbers[i] == 0)
+                {
+                    numbers[i] = 0;
+                }
+                Console.WriteLine($"{numbers[i]} => {roundedNum}");
+            }
+
+        }
+    }
+}
