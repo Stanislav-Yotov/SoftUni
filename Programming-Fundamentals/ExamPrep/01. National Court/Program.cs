e4904e30ec6669eebcdbac183eb15d215f585889
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-07-07 16:50:40 +0300

    Preparation before my Mid-Exam.

---
+++ Programming-Fundamentals/ExamPrep/01. National Court/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstEmployee = int.Parse(Console.ReadLine());
+            int secondEmployee = int.Parse(Console.ReadLine());
+            int thirdEmployee = int.Parse(Console.ReadLine());
+            int peopleCount = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            int employeeEficiency = firstEmployee + secondEmployee + thirdEmployee;
+
+            while (peopleCount > 0)
+            {
+                count++;
+                if (count % 4 == 0)
+                {
+                    continue;
+                }
+
+                peopleCount -= employeeEficiency;
+
+            }
+
+            Console.WriteLine($"Time needed: {count}h.");
+
+
+
+        }
+    }
+}
---
+++ Programming-Fundamentals/ExamPrep/02. Shopping List/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Shopping_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> shoppingList = Console.ReadLine()
+                                               .Split("!")
+                                               .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "Go Shopping!")
+            {
+                string[] cmdArgs = command.Split();
+                if (cmdArgs[0] == "Urgent")
+                {
+                    if (!shoppingList.Contains(cmdArgs[1]))
+                    {
+                        shoppingList.Insert(0, cmdArgs[1]);
+                    }
+                    
+                }
+                else if (cmdArgs[0] == "Unnecessary")
+                {
+                    if (shoppingList.Contains(cmdArgs[1]))
+                    {
+                        shoppingList.Remove(cmdArgs[1]);
+                    }
+                    
+                }
+                else if (cmdArgs[0] == "Correct")
+                {
+                    string newGrocerie = cmdArgs[2];
+                    int index = shoppingList.IndexOf(cmdArgs[1]);
+                    if (shoppingList.Contains(cmdArgs[1]))
+                    {
+                        shoppingList.Insert(index, newGrocerie);
+                        shoppingList.Remove(cmdArgs[1]);
+                    }
+                }
+                else if (cmdArgs[0] == "Rearrange")
+                {
+                    if (shoppingList.Contains(cmdArgs[1]))
+                    {
+                        shoppingList.Remove(cmdArgs[1]);
+                        shoppingList.Add(cmdArgs[1]);
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", shoppingList));
+        }
+    }
+}
