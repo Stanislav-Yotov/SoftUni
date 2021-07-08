01fc5325db7cdc2f23bf8eb467d4a6e762f65c54
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-07-08 16:09:03 +0300

    Preparation.

---
+++ Programming-Fundamentals/ExamPrep1/01. Computer Store/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _01._Computer_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            decimal currentSum = 0m;
+            decimal totalSum = 0m;
+            decimal tax = 0m;
+
+            while (command != "special" && command != "regular")
+            {
+
+                decimal price = decimal.Parse(command);
+
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    command = Console.ReadLine();
+                }
+
+                else
+                {
+                    currentSum += price;
+                    tax = currentSum * 0.2m;
+                    totalSum = currentSum + tax;
+
+                    command = Console.ReadLine();
+                }
+
+            }
+
+            if (totalSum == 0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else if (command == "regular")
+            {
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {currentSum:f2}$");
+                Console.WriteLine($"Taxes: {tax:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {totalSum:f2}$");
+            }
+            else if (command == "special")
+            {
+                totalSum = (currentSum + tax) - ((currentSum + tax) * 0.1m);
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {currentSum:f2}$");
+                Console.WriteLine($"Taxes: {tax:f2}$");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"Total price: {totalSum:f2}$");
+            }
+
+
+        }
+    }
+}
---
+++ Programming-Fundamentals/ExamPrep1/02. The Lift/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._The_Lift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waitingPeople = int.Parse(Console.ReadLine());
+            int[] wagon = Console.ReadLine()
+                                      .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                      .Select(int.Parse)
+                                      .ToArray();
+
+            int peopleOnCurrentWagon = 0;
+            int peopleOnTheLift = 0;
+            bool noMorePeople = false;
+            for (int i = 0; i < wagon.Length; i++)
+            {
+                while (wagon[i] < 4)
+                {
+                    wagon[i]++;
+                    peopleOnCurrentWagon++;
+                    if (peopleOnTheLift + peopleOnCurrentWagon == waitingPeople)
+                    {
+                        noMorePeople = true;
+                        break;
+                    }
+                }
+                peopleOnTheLift += peopleOnCurrentWagon;
+                if (noMorePeople)
+                {
+                    break;
+                }
+                peopleOnCurrentWagon = 0;
+            }
+
+            if (waitingPeople > peopleOnTheLift)
+            {
+                Console.WriteLine($"There isn't enough space! {waitingPeople - peopleOnTheLift} people in a queue!");
+                Console.WriteLine(string.Join(" ", wagon));
+            }
+            else if (waitingPeople < wagon.Length * 4 && wagon.Any(w => w < 4))
+            {
+                Console.WriteLine("The lift has empty spots!");
+                Console.WriteLine(string.Join(" ", wagon));
+            }
+            else if (wagon.All(w => w == 4) && noMorePeople == true)
+            {
+                Console.WriteLine(string.Join(" ", wagon));
+            }
+        }
+    }
+}
