5bc7d8bce7181e9f07d28d8162ee935a6fb486da
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-07-17 12:28:21 +0300

    Dictionaries in C#.

---
+++ Programming-Fundamentals/associativeArrays/02. Odd Occurrences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                                    .ToLower()
+                                    .Split();
+
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (counts.ContainsKey(word))
+                {
+                    counts[word]++;
+                }
+                else
+                {
+                    counts.Add(word, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+                                    
+        }
+    }
+}
