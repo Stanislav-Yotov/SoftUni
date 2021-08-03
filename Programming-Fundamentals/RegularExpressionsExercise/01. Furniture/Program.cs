52c79d189958a5e00d126799d8136c2dc8f7a484
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-08-03 20:10:01 +0300

    regex exercise

---
+++ Programming-Fundamentals/RegularExpressionsExercise/01. Furniture/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>([A-Za-z]+)<<(\d+\.?\d*)!(\d+)";
+
+            string input = Console.ReadLine();
+
+            decimal sum = 0;
+
+            Console.WriteLine("Bought furniture:");
+
+            while (input != "Purchase")
+            {
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    string productName = match.Groups[1].Value;
+                    decimal productPrice = decimal.Parse(match.Groups[2].Value);
+                    int productQuantity = int.Parse(match.Groups[3].Value);
+                    Console.WriteLine(productName);
+
+                    sum += productPrice * productQuantity;
+
+                }
+
+                input = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
---
+++ Programming-Fundamentals/RegularExpressionsExercise/02. Race/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] listOfPeople = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> dictionaryOfNames = new Dictionary<string, int>();
+
+            foreach (var name in listOfPeople)
+            {
+                dictionaryOfNames.Add(name, 0);
+            }
+
+            string namePattern = @"[\W\d]";
+            string numberPattern = @"[\WA-Za-z]";
+
+            string input = Console.ReadLine();
+
+            while (input != "end of race")
+            {
+                string name = Regex.Replace(input, namePattern, "");
+                string distance = Regex.Replace(input, numberPattern, "");
+
+                int sum = 0;
+
+                foreach (var digit in distance)
+                {
+                    int currentDigit = int.Parse(digit.ToString());
+                    sum += currentDigit;
+                }
+
+                if (dictionaryOfNames.ContainsKey(name))
+                {
+                    dictionaryOfNames[name] += sum;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            int counter = 1;
+
+            foreach (var kvp in dictionaryOfNames.OrderByDescending(x => x.Value))
+            {
+                string text = counter == 1 ? "st" : counter == 2 ? "nd" : "rd";
+
+                Console.WriteLine($"{counter++}{text} place: {kvp.Key}");
+
+                if (counter == 4)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
