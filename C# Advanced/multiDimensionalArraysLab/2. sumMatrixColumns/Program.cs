e8edf228c820ac4f53865644a98d9b084ddaec2a
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-09-11 20:55:56 +0300

    Multidimensional Arrays lab

---
+++ C# Advanced/multiDimensionalArraysLab/2. sumMatrixColumns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._sumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine()
+                                 .Split(", ")
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            for (int row = 0;  row < sizes[0];  row++)
+            {
+                int[] rows = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < sizes[1]; col++)
+                {
+                    matrix[row, col] = rows[col];
+                }
+            }
+
+            for (int i = 0; i < sizes[1]; i++)
+            {
+                int columnSum = 0;
+                for (int j = 0; j < sizes[0]; j++)
+                {
+                    columnSum += matrix[j, i];
+                }
+
+                Console.WriteLine(columnSum);
+            }
+        }
+    }
+}
---
+++ C# Advanced/multiDimensionalArraysLab/3. Primary Diagonal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _3._primaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            int sum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = rowNums[col];
+                    if (row == col)
+                    {
+                        sum += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
