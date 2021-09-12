8b31c9d8fb00d53291b38c7d5099c1fb3f4af0d1
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-09-12 11:18:44 +0300

    Multidimensional Arrays exercise

---
+++ C# Advanced/multidimensionalArraysExercise/1. diagonalDifference/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._diagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = values[col];
+                }
+            }
+
+            int primaryDiagonal = 0;
+            int secondaryDiagonal = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                primaryDiagonal += matrix[row, row];
+                secondaryDiagonal += matrix[row, n - row - 1];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
+        }
+    }
+}
