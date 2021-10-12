06105e6ba9cb757a1b125553e43ce0a704afef39
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-10-12 11:40:01 +0300

    update

---
+++ C# Advanced/genericsExercise/Tuple/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            string name = $"{firstInput[0]} {firstInput[1]}";
+            string adress = firstInput[2];
+            Tuple<string, string> firstTuple = new Tuple<string, string>(name, adress);
+            Console.WriteLine(firstTuple);
+            string[] secondInput = Console.ReadLine().Split();
+            string secondName = secondInput[0];
+            int liters = int.Parse(secondInput[1]);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(secondName, liters);
+            Console.WriteLine(secondTuple);
+            string[] thirdInput = Console.ReadLine().Split();
+            int integer = int.Parse(thirdInput[0]);
+            double doubleNum = double.Parse(thirdInput[1]);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(integer, doubleNum);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ C# Advanced/genericsExercise/Tuple/Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+   public class Tuple<T, V>
+    {
+        public Tuple(T item1, V item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public T Item1 { get; set; }
+        public V Item2 { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Item1} -> {Item2}";
+        }
+    }
+}
