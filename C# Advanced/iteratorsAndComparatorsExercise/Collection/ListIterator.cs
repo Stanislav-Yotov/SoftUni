ae0e6de0cb43cb2b98f36245a07fbbd7a4b9ffaf
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-10-15 12:53:14 +0300

    Iterators and comparators exercise.

---
+++ C# Advanced/iteratorsAndComparatorsExercise/Collection/ListIterator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collection
+{
+    public class ListIterator<T> : IEnumerable<T>
+    {
+        private List<T> list;
+        private int currentIndex;
+
+        public ListIterator(params T[] list)
+        {
+            this.list = new List<T>(list);
+
+            currentIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (currentIndex + 1 < list.Count)
+            {
+                currentIndex++;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasNext() => currentIndex + 1 < list.Count;
+
+        public void Print()
+        {
+            if (list.Count > 0 && currentIndex < list.Count)
+            {
+                Console.WriteLine(list[currentIndex]);
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+        }
+
+        public void PrintAll()
+        {
+            Console.WriteLine(string.Join(" ", list));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- C# Advanced/iteratorsAndComparatorsExercise/Collection/Program.cs
+++ C# Advanced/iteratorsAndComparatorsExercise/Collection/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Linq;
 
 namespace Collection
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            ListIterator<string> list = null;
+
+            string line = Console.ReadLine();
+
+            while (line != "END")
+            {
+                try
+                {
+                    string[] commandData = line
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    string command = commandData[0];
+
+                    if (command == "Create")
+                    {
+                        list = new ListIterator<string>(commandData.Skip(1).ToArray());
+                    }
+                    else if (command == "Move")
+                    {
+                        Console.WriteLine(list.Move());
+                    }
+                    else if (command == "Print")
+                    {
+                        list.Print();
+                    }
+                    else if (command == "HasNext")
+                    {
+                        Console.WriteLine(list.HasNext());
+                    }
+                    else if (command == "PrintAll")
+                    {
+                        list.PrintAll();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                line = Console.ReadLine();
+            }
         }
     }
 }
