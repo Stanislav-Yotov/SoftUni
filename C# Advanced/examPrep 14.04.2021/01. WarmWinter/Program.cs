b35414a4fed037e56c0ead32643cdc8143f4135c
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-10-20 17:06:41 +0300

    exam prep 14.04.2021

---
+++ C# Advanced/examPrep 14.04.2021/01. WarmWinter/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._WarmWinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> hats = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            List<int> set = new List<int>();
+
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                var hat = hats.Peek();
+                var scarf = scarfs.Peek();
+                if (hat > scarf)
+                {
+                    int result = hat + scarf;
+                    set.Add(result);
+                    hats.Pop();
+                    scarfs.Dequeue();
+                }
+                else if (scarf > hat)
+                {
+                    hats.Pop();
+                }
+                else if (hat == scarf)
+                {
+                    scarfs.Dequeue();
+                    int result = hat + 1;
+                    hats.Pop();
+                    hats.Push(result);
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {set.Max()}");
+            foreach (var item in set)
+            {
+                Console.Write(string.Join(" ", item + " "));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ C# Advanced/examPrep 14.04.2021/02. SuperMario/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace _02._SuperMario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lives = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            char[][] field = new char[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                char[] chars = Console.ReadLine().ToCharArray();
+                field[i] = chars;
+            }
+
+            int marioRow = 0;
+            int marioCol = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    if (field[i][j] == 'M')
+                    {
+                        marioRow = i;
+                        marioCol = j;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                string[] inputArgs = input.Split();
+                string command = inputArgs[0];
+                int bowserRow = int.Parse(inputArgs[1]);
+                int bowserCol = int.Parse(inputArgs[2]);
+
+                lives--;
+                field[bowserRow][bowserCol] = 'B';
+                field[marioRow][marioCol] = '-';
+
+                // Move
+                if (command == "W" && marioRow - 1 >= 0)
+                {
+                    marioRow--;
+                }
+                else if (command == "S" && marioRow + 1 < rows)
+                {
+                    marioRow++;
+                }
+                else if (command == "A" && marioCol - 1 >= 0)
+                {
+                    marioCol--;
+                }
+                else if (command == "D" && marioCol + 1 < field[marioRow].Length)
+                {
+                    marioCol++;
+                }
+
+                //after moving
+                if (field[marioRow][marioCol] == 'B')
+                {
+                    lives -= 2;
+                }
+                if (field[marioRow][marioCol] == 'P')
+                {
+                    field[marioRow][marioCol] = '-';
+                    Console.WriteLine($"Mario has successfully saved the princess! Lives left: {lives}");
+                    break;
+                }
+                if (lives <= 0)
+                {
+                    field[marioRow][marioCol] = 'X';
+                    Console.WriteLine($"Mario died at {marioRow};{marioCol}.");
+                    break;
+                }
+                field[marioRow][marioCol] = 'M';
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.WriteLine(new string(field[i]));
+            }
+        }
+    }
+}
