46cb42bd4c1311969053ff7f84c5e5622a2649aa
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-10-21 09:55:37 +0300

    exam prep 22.02.2020

---
+++ C# Advanced/examPrep22.02.2020/01. Lootbox/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstBox = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+            Stack<int> secondBox = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            List<int> items = new List<int>();
+
+            while (firstBox.Count > 0 && secondBox.Count > 0)
+            {
+                var first = firstBox.Peek();
+                var second = secondBox.Peek();
+                var sum = first + second;
+
+                if (sum % 2 == 0)
+                {
+                    items.Add(sum);
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    secondBox.Pop();
+                    firstBox.Enqueue(second);
+                }
+            }
+
+            if (firstBox.Count <= 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+
+            if (items.Sum() >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {items.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {items.Sum()}");
+            }
+
+
+        }
+    }
+}
---
+++ C# Advanced/examPrep22.02.2020/02. Re-Volt/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02._Re_Volt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int commandsCount = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+            for (int row = 0; row < n; row++)
+            {
+                string currentRow = Console.ReadLine();
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            int playerRow = 0;
+            int playerCol = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == 'f')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+
+            while (commandsCount > 0)
+            {
+
+            }
+
+
+            
+        }
+    }
+}
