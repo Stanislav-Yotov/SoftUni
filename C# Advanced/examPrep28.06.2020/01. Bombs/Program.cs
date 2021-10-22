66af062fedf6880b55011b5b01932de7b41dce3e
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-10-22 20:33:56 +0300

    exam prep 28.06.2020

---
+++ C# Advanced/examPrep28.06.2020/01. Bombs/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> bombEfects = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> bombCasings = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int daturaBombs = 0, cherryBombs = 0, smokeDecoyBombs = 0;
+            bool isReady = false;
+
+            while (bombEfects.Count > 0 && bombCasings.Count > 0)
+            {
+                int bombEfect = bombEfects.Peek();
+                int bombCasing = bombCasings.Peek();
+                if (daturaBombs >= 3 && cherryBombs >= 3 && smokeDecoyBombs >= 3)
+                {
+                    isReady = true;
+                    break;
+                }
+
+                if (bombEfect + bombCasing == 40)
+                {
+                    daturaBombs++;
+                    bombCasings.Pop();
+                    bombEfects.Dequeue();
+                }
+                else if (bombEfect + bombCasing == 60)
+                {
+                    cherryBombs++;
+                    bombCasings.Pop();
+                    bombEfects.Dequeue();
+                }
+                else if (bombEfect + bombCasing == 120)
+                {
+                    smokeDecoyBombs++;
+                    bombCasings.Pop();
+                    bombEfects.Dequeue();
+                }
+                else
+                {
+                    bombCasing -= 5;
+                    bombCasings.Pop();
+                    bombCasings.Push(bombCasing);
+                }
+            }
+
+            if (isReady)
+            {
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+            }
+            if (bombEfects.Any())
+            {
+                Console.Write("Bomb Effects:" + " ");
+                Console.WriteLine(string.Join(", ", bombEfects));
+            }
+            else
+            {
+                Console.WriteLine("Bomb Effects: empty");
+            }
+            if (bombCasings.Any())
+            {
+                Console.Write("Bomb Casings:" + " ");
+                Console.WriteLine(string.Join(", ", bombCasings));
+            }
+            else
+            {
+                Console.WriteLine("Bomb Casings: empty");
+            }
+            Console.WriteLine($"Cherry Bombs: {cherryBombs}");
+            Console.WriteLine($"Datura Bombs: {daturaBombs}");
+            Console.WriteLine($"Smoke Decoy Bombs: {smokeDecoyBombs}");
+
+
+
+        }
+    }
+}
---
+++ C# Advanced/examPrep28.06.2020/02. Snake/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] field = new char[n, n];
+            int snakeRow = -1;
+            int snakeCol = -1;
+            List<int[]> burrows = new List<int[]>();
+            for (int row = 0; row < n; row++)
+            {
+                var currentRow = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    field[row, col] = currentRow[col];
+                    if (field[row, col] == 'S')
+                    {
+                        snakeRow = row;
+                        snakeCol = col;
+                    }
+                    if (field[row, col] == 'B')
+                    {
+                        burrows.Add(new int[] { row, col });
+                    }
+                }
+            }
+
+            int foodCounter = 0;
+            string input = Console.ReadLine();
+            while (true)
+            {
+               
+                field[snakeRow, snakeCol] = '.';
+                snakeRow = MoveRow(snakeRow, input);
+                snakeCol = MoveCol(snakeCol, input);
+
+                if (!IsPositionValid(snakeRow, snakeCol, n,n))
+                {
+                    Console.WriteLine("Game over!");
+                    break;
+                }
+
+                if (field[snakeRow, snakeCol] == '*')
+                {
+                    foodCounter++;
+                    field[snakeRow, snakeCol] = '.';
+                }
+                else if (field[snakeRow, snakeCol] == 'B')
+                {
+                    foreach (var currentBurrow in burrows)
+                    {
+                        int currentRow = currentBurrow[0];
+                        int currentCol = currentBurrow[1];
+                        field[currentRow, currentCol] = '.';
+                    }
+
+                    int[] newIndexes = burrows[1];
+                    int newRow = newIndexes[0];
+                    int newCol = newIndexes[1];
+                    field[newRow, newCol] = 'S';
+                    snakeRow = newRow;
+                    snakeCol = newCol;
+                }
+                field[snakeRow, snakeCol] = 'S';
+                if (foodCounter >= 10)
+                {
+                    Console.WriteLine("You won! You fed the snake.");
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Food eaten: {foodCounter}");
+
+            for (int row = 0; row < field.GetLength(0); row++)
+            {
+                for (int col = 0; col < field.GetLength(1); col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static bool IsPositionValid(int row, int col, int rows, int cols)
+        {
+            if (row < 0 || row >= rows)
+            {
+                return false;
+            }
+            if (col < 0 || col >= cols)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int MoveRow(int row, string movement)
+        {
+            if (movement == "up")
+            {
+                return row - 1;
+            }
+            if (movement == "down")
+            {
+                return row + 1;
+            }
+
+            return row;
+        }
+
+        public static int MoveCol(int col, string movement)
+        {
+            if (movement == "left")
+            {
+                return col - 1;
+            }
+            if (movement == "right")
+            {
+                return col + 1;
+            }
+
+            return col;
+        }
+    }
+}
