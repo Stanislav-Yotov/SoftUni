924de56a8a1062901d4b921586d440554f380c24
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-10-25 17:33:41 +0300

    basic algorithms

---
+++ C# Advanced/basicAlgorithms/RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            int curentIndex = 0;
+            Console.WriteLine(Sum(arr, curentIndex, sum));
+        }
+
+        private static int Sum(int[] arr, int index, int sum)
+        {
+            if (index == arr.Length)
+            {
+                return sum;
+            }
+            sum += arr[index];
+            index++;
+
+            return Sum(arr, index, sum);
+        }
+    }
+}
---
+++ C# Advanced/basicAlgorithms/RecursiveFactorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double result = 1;
+            Console.WriteLine(Factorial(n, result));
+        }
+
+        static double Factorial(double x, double result)
+        {
+
+            if (x <= 1)
+            {
+                return result;
+            }
+
+            result *= x;
+            x--;
+            return Factorial(x, result);
+        }
+    }
+}
