19875e94d8fd23d7af098e7c8fb78c99d0d157c6
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-10-26 12:55:06 +0300

    Inheritance lab.

---
+++ C# OOP/inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool isEmpty => this.Count == 0;
+
+        public Stack<string> AddRange(IEnumerable<string> collection)
+        {
+            foreach (var str in collection)
+            {
+                this.Push(str);
+            }
+            return this;
+        }
+    }
+}
