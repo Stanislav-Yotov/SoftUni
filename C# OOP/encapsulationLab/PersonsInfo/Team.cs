9ce6b9294cd11b2bcabf66d6e38e667f78f6abab
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-10-30 13:21:59 +0300

    encapsulation lab.

---
+++ C# OOP/encapsulationLab/PersonsInfo/Team.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyList<Person> FirstTeam  
+        {
+            get { return firstTeam.AsReadOnly(); } 
+        }
+
+        public IReadOnlyList<Person> ReserveTeam 
+        {
+            get { return reserveTeam.AsReadOnly(); }
+        }
+
+        public string Name { get => name; set => name = value; }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age <= 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
