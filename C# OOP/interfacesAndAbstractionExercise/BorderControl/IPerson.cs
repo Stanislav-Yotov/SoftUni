38fa1552b0d5d5def86592bf37ea3b6be9e8158f
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-02 15:47:19 +0200

    Interfaces and abstractions exercise.

---
+++ C# OOP/interfacesAndAbstractionExercise/BorderControl/IPerson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public interface IPerson
+    {
+        public string Name { get;}
+        public int Age { get;}
+    }
+}
---
+++ C# OOP/interfacesAndAbstractionExercise/BorderControl/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BorderControl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IIdentifiable> identifiables = new List<IIdentifiable>();
+            string input;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] identifiableData = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                IIdentifiable identifiable;
+                if (identifiableData.Length == 3)
+                {
+                    string name = identifiableData[0];
+                    int age = int.Parse(identifiableData[1]);
+                    string id = identifiableData[2];
+
+                    identifiable = new Citizen(name, age, id);
+                }
+                else
+                {
+                    string model = identifiableData[0];
+                    string id = identifiableData[1];
+
+                    identifiable = new Robot(model, id);
+                }
+
+                identifiables.Add(identifiable);
+
+                //input = Console.ReadLine();
+            }
+
+            string lastDigits = Console.ReadLine();
+
+            foreach (var identifiable in identifiables)
+            {
+                if (identifiable.Id.EndsWith(lastDigits))
+                {
+                    Console.WriteLine(identifiable.Id);
+                }
+            }
+        }
+    }
+}
