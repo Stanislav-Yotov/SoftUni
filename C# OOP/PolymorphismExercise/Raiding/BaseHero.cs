fe5f76976415176a15f4c8637f94c439e2295d79
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-09 13:08:48 +0200

    Polymorphism exercise.

---
+++ C# OOP/PolymorphismExercise/Raiding/BaseHero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        public BaseHero(string name, int power)
+        {
+            Name = name;
+            Power = power;
+        }
+
+        public string Name { get; set; }
+
+        public int Power { get; set; }
+
+        public abstract void CastAbility();
+        
+    }
+}
