37cc4b96483357cb6ba59474055694834d4f6027
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-09 15:37:24 +0200

    Polymorphism exercise.

---
+++ C# OOP/PolymorphismExercise/WildFarm/Animals/Cat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WildFarm.Foods;
+
+namespace WildFarm.Animals
+{
+    public class Cat : Feline
+    {
+        private const double DefaultWeightModifier = 0.3;
+
+        private static HashSet<string> allowedFoods = new HashSet<string>
+        {
+            nameof(Meat),
+            nameof(Vegetables)
+        };
+
+        public Cat(string name, double weight, string livingRegion, string breed) : base(name, weight, livingRegion, allowedFoods, DefaultWeightModifier, breed)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return $"Meow";
+        }
+    }
+}
---
+++ C# OOP/PolymorphismExercise/WildFarm/Foods/Vegetables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm.Foods
+{
+    public class Vegetables : Food
+    {
+        public Vegetables(int quantity) : base(quantity)
+        {
+        }
+    }
+}
