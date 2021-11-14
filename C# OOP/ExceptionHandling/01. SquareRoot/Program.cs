781bb555fa2d016791de3434279770d6cf912f96
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-14 16:29:14 +0200

    Exception handling.

---
+++ C# OOP/ExceptionHandling/01. SquareRoot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _01._SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string n = Console.ReadLine();
+                ValidateNumber(n);
+                double square = Math.Sqrt(double.Parse(n));
+                Console.WriteLine(square);
+            }
+            catch (ArgumentException)
+            {
+
+                Console.WriteLine("Invalid number");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye");
+            }
+
+
+        }
+
+        private static void ValidateNumber(string n)
+        {
+            if (int.Parse(n) < 0)
+            {
+                throw new ArgumentException();
+            }
+            if (!int.TryParse(n, out _))
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
