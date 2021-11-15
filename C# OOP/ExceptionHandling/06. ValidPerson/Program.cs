8a4b11b6105a17e667e7d80857dce4aaeca8f6c7
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-15 11:48:16 +0200

    Exception handling

---
+++ C# OOP/ExceptionHandling/06. ValidPerson/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _06._ValidPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person pesho = new Person("petyr", "peshov", 80);
+
+            }
+            catch (ArgumentNullException e)
+            { 
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Person gosho = new Person("", "goshov", 60);
+            }
+            catch (ArgumentNullException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Person dimo = new Person("dimo", "dimov", -1);
+            }
+            catch (ArgumentNullException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Person simo = new Person("simo", "simov", 121);
+            }
+            catch (ArgumentNullException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
