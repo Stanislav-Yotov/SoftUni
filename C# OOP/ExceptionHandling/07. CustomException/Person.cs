8a71338ebc246d5c6b798fa2f8c6b83477889d7e
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-15 15:01:06 +0200

    Exception handling.

---
+++ C# OOP/ExceptionHandling/07. CustomException/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07._CustomException
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("First name can't be null or empty!");
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Last name can't be null or empty!");
+                }
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set
+            {
+                if (value < 0 || value > 120)
+                {
+                    throw new ArgumentOutOfRangeException("Age should be int range [0 - 120]!");
+                }
+                age = value;
+            }
+        }
+    }
+}
---
+++ C# OOP/ExceptionHandling/07. CustomException/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _07._CustomException
+{
+    public class Student
+    {
+        private string name;
+        private string email;
+
+        public Student(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public string Name 
+        {
+            get => name;
+            set
+            {
+                if (value.Any(c => !char.IsLetter(c)))
+                {
+                    throw new InvalidPersonNameException("Name must contain only letters");
+                }
+                value = name;
+            }
+        }
+
+        public string Email { get; set; }
+    }
+}
