c70aaecfa3f1caab05b33fd6189a85fcce9ff3b3
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-15 10:34:07 +0200

    Exception handling

---
+++ C# OOP/ExceptionHandling/05. ConvertToDouble/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05._ConvertToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            double result;
+            foreach (var item in input)
+            {
+                try
+                {
+
+                    result = Convert.ToDouble(item);
+                    if (double.IsPositiveInfinity(result) || double.IsNegativeInfinity(result))
+                    {
+                        throw new OverflowException();
+                    }
+                    Console.WriteLine($"Converted '{item}' to {result}");
+                }
+                catch (FormatException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
