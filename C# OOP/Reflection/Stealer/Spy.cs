fe670352726458d7b7b057783d0d7d594fe43792
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-18 10:48:45 +0200

    Reflection lab.

---
+++ C# OOP/Reflection/Stealer/Spy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+         public string StealFieldInfo(string className, params string[] fieldNames)
+         {
+
+             Type classType = Type.GetType(className);
+             FieldInfo[] fields = classType.GetFields(BindingFlags.Instance | BindingFlags.Public |
+                 BindingFlags.NonPublic | BindingFlags.Static);
+             StringBuilder sb = new StringBuilder();
+             Object instanced = Activator.CreateInstance(classType, new object[] { });
+             sb.AppendLine($"Class under investigation: {className}");
+             foreach (FieldInfo field in fields.Where(f => fieldNames.Contains(f.Name)))
+             {
+                sb.AppendLine($"{field.Name} = {field.GetValue(instanced)}");
+             }
+
+            return sb.ToString().TrimEnd();
+         }
+
+       
+    }
+}
