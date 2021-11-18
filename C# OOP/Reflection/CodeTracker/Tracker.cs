6a9b8b80296f686b582af33b06e5c65febb4f40a
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-18 11:35:58 +0200

    Reflection lab.

---
+++ C# OOP/Reflection/CodeTracker/Tracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            StringBuilder sb = new StringBuilder();
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance |
+                BindingFlags.Static | BindingFlags.NonPublic);
+            foreach (var method in methods)
+            {
+                if (method.CustomAttributes.Any(a => a.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+                    foreach (var attribute in attributes)
+                    {
+                        sb.AppendLine($"{method.Name} is written by {(attribute as AuthorAttribute).Name}");
+                    }
+                }
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
