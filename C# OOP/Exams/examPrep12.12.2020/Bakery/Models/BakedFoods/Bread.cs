2b2b25cf599a443748d3276f79b26205d1f00f7c
Stanislav Yotov <stanislav.iotov@abv.bg>
2021-11-28 19:04:25 +0200

    Exam 12.12.2020/ Structure and Business logic./134 points in Judge. Best I can do :)

---
+++ C# OOP/Exams/examPrep12.12.2020/Bakery/Models/BakedFoods/Bread.cs
@@ -0,0 +1,16 @@
+﻿using Bakery.Models.BakedFoods;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models
+{
+    public class Bread : BakedFood
+    {
+        private const int initialBreadPortion = 200;
+
+        public Bread(string name, decimal price) : base(name, initialBreadPortion, price)
+        {
+        }
+    }
+}
