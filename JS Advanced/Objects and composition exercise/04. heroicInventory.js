93764b5929bde56b8272458e7de5b9f7e93e81cb
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-01-20 11:05:33 +0200

    objects and composition exercise

---
+++ JS Advanced/Objects and composition exercise/04. heroicInventory.js
@@ -0,0 +1,17 @@
+function solve(input){
+    let result = [];
+
+    for (const iterator of input) {
+        let [name, level, items] = iterator.split(' / ');
+        level = Number(level);
+        items = items ? items.split(', ') : [];
+
+        result.push({name, level, items});
+    }
+
+    return JSON.stringify(result);
+}
+
+console.log(solve(['Isacc / 25 / Apple, GravityGun',
+                   'Derek / 12 / BarrelVest, DestructionSword',
+                   'Hes / 1 / Desolator, Sentinel, Antara']));
