f40fb17fd0797534e44144e6d63b367a61364764
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-01-22 16:03:15 +0200

    DOM introduction

---
+++ JS Advanced/DOM introduction/02.Collect List Items/CollectListItems.js
@@ -0,0 +1,7 @@
+function extractText() {
+    let ulElement = document.getElementById('items');
+
+    let textAreaElement = document.getElementById('result')
+
+    textAreaElement.textContent = ulElement.textContent;
+}
