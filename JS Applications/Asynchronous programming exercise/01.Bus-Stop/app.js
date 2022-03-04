7090497d57ead01dd03e84a0cfb0a4ff01779644
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-03-04 18:43:36 +0200

    exercise

---
+++ JS Applications/Asynchronous programming exercise/01.Bus-Stop/app.js
@@ -0,0 +1,26 @@
+async function getInfo() {
+    let stopId = document.getElementById('stopId').value;
+    let timetable = document.getElementById('buses');
+    let stopName = document.getElementById('stopName');
+    let url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+
+    try {
+        timetable.replaceChildren();
+        let response = await fetch(url);
+        if (response.ok === false) {
+            throw new Error('Error');
+        }
+        let data = await response.json();
+      
+        stopName.textContent = data.name;
+        Object.entries(data.buses).forEach(b => {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`;
+            timetable.appendChild(li);
+        });
+    } catch (error) {
+        stopName.textContent = 'Error';
+    }
+
+
+}
